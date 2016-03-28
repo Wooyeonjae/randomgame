932eba70ff0b4fb99d597b8c421564f9d2b62b59
Yeonjae Woo <yeonjae0331@naver.com>
2016-03-28 20:54:54 +0900

    #5 implement TextView Result Activity

--- app/src/main/java/kr/hs/emirim/yeonjae0331/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/yeonjae0331/monchicken/ResultActivity.java
@@ -2,13 +2,21 @@ package kr.hs.emirim.yeonjae0331.monchicken;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 
 public class ResultActivity extends AppCompatActivity {
+
+    TextView mResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mResult = (TextView) fineViewById(R.id.result);
+        mResult.setText("결과");
     }
+
+
 }
