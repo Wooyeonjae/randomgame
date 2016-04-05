8234eeab08f81b462c9906231ccc38e28f1d6bab
Yeonjae Woo <yeonjae0331@naver.com>
2016-04-05 19:05:04 +0900

    #4 add intent code

--- app/src/main/java/kr/hs/emirim/yeonjae0331/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/yeonjae0331/monchicken/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.yeonjae0331.monchicken;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,5 +37,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         Toast.makeText(this, "배고파요!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
