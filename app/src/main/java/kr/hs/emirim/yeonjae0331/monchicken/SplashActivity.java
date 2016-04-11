c25787ac29557be20dae72e038fee8dbe9d5b3d9
Yeonjae Woo <yeonjae0331@naver.com>
2016-04-11 21:01:29 +0900

    #7 Create Splash Activity

---
+++ app/src/main/java/kr/hs/emirim/yeonjae0331/monchicken/SplashActivity.java
@@ -0,0 +1,26 @@
+package kr.hs.emirim.yeonjae0331.monchicken;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);   //3000밀리세컨드 ->3초
+    }
+}
